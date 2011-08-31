526b34148b7c8ff360d6e470b18eaf5fbf02af80
oeo4b <omar@omar.com>
2011-08-30 22:45:06 -0400

    still working on improving the kriging plot method

--- R/plot.R
+++ R/plot.R
@@ -1,7 +1,16 @@
 #
-# Plotting methods
+# Plotting methods for kriging class
 #
 
 setMethod("plot", 
-  signature(x="kriging")
+  signature(x="kriging", y="missing"),
+  function(x, y, col = terrain.colors(20), title = NA, ...) {
+    # Set up the color spectrum
+    cols <- x@data$pred.pred - min(x@data$pred.pred)
+    cols <- cols * 1/max(cols)
+    cols <- col[length(col)*cols]    
+
+    # Use the base plot method
+    plot(x@coords, col = cols)
+  }
 )
